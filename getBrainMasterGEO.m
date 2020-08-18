3fecc1b38b0ca43ea0d3ec33eb9442924da91bf7
aman-s <40581215+aman-s@users.noreply.github.com>
2020-08-18 13:45:45 -0700

    Update getBrainMasterGEO.m

--- getBrainMasterGEO.m
+++ getBrainMasterGEO.m
@@ -15,16 +15,14 @@ for i = 1:length(varargin)
             runGemma = double(varargin{i+1,1});
         elseif string(varargin{i,1}) == "numThreads"
             numWorkers = double(varargin{i+1,1});
-        elseif string(varargin{i,1}) == "gemmaLasteeID"
-            lastGemmaID = double(varargin{i+1,1});
-        else disp(strcat("Typo found in name of parameter: ",varargin{i,1}, ". Use one of: 'fileToSkip', 'runGemma', 'numThreads', or 'gemmaLasteeID'"));
+        else disp(strcat("Typo found in name of parameter: ",varargin{i,1}, ". Use one of: 'fileToSkip', 'runGemma', or 'numThreads'"));
             if get(0, 'ScreenSize') == [1,1,1024,768] exit(), else return;,end
         end
     end
 end
 % commented line: cd /space/grp/asharma/getBrainExps : for github use, if you want to save the getBrainMaster in a different location, add the cd line with the folder containing all the rest of the files
 cd src
-y = getBrainExpsFromGEO(geoStart,geoEnd, 1,myfilename,numWorkers, runGemma, userDir, lastGemmaID);
+y = getBrainExpsFromGEO(geoStart,geoEnd, 1,myfilename,numWorkers, runGemma, userDir);
 cd(userDir)
 if get(0, 'ScreenSize') == [1,1,1024,768] exit(), else return;,end
 end
